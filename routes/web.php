349f869c16fcd11288481ffc177fb9cfd3b34cab
Ronnie Isurina <ronnie.isurina@gmail.com>
2021-01-23 13:53:11 +0800

    Setup vagrant & homestead.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return "LOGIN";
+})->name('login');
+
+
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware('auth')->name('dashboard');
